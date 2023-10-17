de9919c3cea958618a182cd53c43c54e25f6a5f0
krystianwarda <krystian.warda@gmail.com>
2023-10-17 01:32:54 -0700

    progress in getCreatureId #2

--- repos/003_send_data/003_send_data/dllmain.cpp
+++ repos/003_send_data/003_send_data/dllmain.cpp
@@ -4,18 +4,16 @@
 #include <TlHelp32.h>
 #include <string>
 
-
-bool IsValidMemoryAddress(void* address) {
-    MEMORY_BASIC_INFORMATION mbi;
-    if (VirtualQuery(address, &mbi, sizeof(mbi))) {
-        DWORD mask = PAGE_READONLY | PAGE_READWRITE | PAGE_WRITECOPY | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY;
-        bool isReadable = (mbi.Protect & mask) != 0;
-        return isReadable && mbi.State == MEM_COMMIT;
+bool CanReadMemory(void* address) {
+    __try {
+        volatile char check = *(char*)address;
+        return true;
     }
-    return false;
+    __except (EXCEPTION_EXECUTE_HANDLER) {
+        return false;
+    }
 }
 
-// Define the Item class as described earlier
 class Item {
 public:
     int m_position_x;
@@ -27,16 +25,20 @@ public:
     Item(int id) : itemId(id) {}
 };
 
-typedef void* (__thiscall* _GetLocalPlayerFunc)(void* gamePtr);
+typedef void* (__thiscall* _GetCreatureByIdFunc)(void* mapPtr, int creatureId);
 typedef void(__thiscall* _SetInventoryItemFunc)(void* localPlayerPtr, int slot, const Item& item);
-typedef void* (__thiscall* _GetCreatureByIdFunc)(void* mapPtr, int creatureId);  // typedef for getCreatureById
+typedef void* (__thiscall* _GetLocalPlayerFunc)(void* gamePtr);
 
 _GetLocalPlayerFunc GetLocalPlayerFunc;
 _SetInventoryItemFunc SetInventoryItemFunc;
-_GetCreatureByIdFunc GetCreatureByIdFunc;  // Function pointer instance
+_GetCreatureByIdFunc GetCreatureByIdFunc;
 
-DWORD WINAPI HackThread(HMODULE hModule)
-{
+const uintptr_t OFFSET_SetInventoryItemFunc = 0x927A0;
+const uintptr_t OFFSET_GetLocalPlayerFunc = 0x5AE30;
+const uintptr_t OFFSET_GetCreatureByIdFunc = 0x14DE10;
+const uintptr_t OFFSET_g_map = 0x932AF0;
+
+DWORD WINAPI HackThread(HMODULE hModule) {
     AllocConsole();
     FILE* f;
     freopen_s(&f, "CONOUT$", "w", stdout);
@@ -45,16 +47,12 @@ DWORD WINAPI HackThread(HMODULE hModule)
 
     uintptr_t moduleBase = 0;
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (hSnapshot != INVALID_HANDLE_VALUE)
-    {
+    if (hSnapshot != INVALID_HANDLE_VALUE) {
         PROCESSENTRY32 pe32;
         pe32.dwSize = sizeof(PROCESSENTRY32);
-        if (Process32First(hSnapshot, &pe32))
-        {
-            do
-            {
-                if (wcsstr(pe32.szExeFile, L"eale"))
-                {
+        if (Process32First(hSnapshot, &pe32)) {
+            do {
+                if (wcsstr(pe32.szExeFile, L"eale")) {
                     moduleBase = (uintptr_t)GetModuleHandleW(pe32.szExeFile);
                     break;
                 }
@@ -63,8 +61,7 @@ DWORD WINAPI HackThread(HMODULE hModule)
         CloseHandle(hSnapshot);
     }
 
-    if (moduleBase == 0)
-    {
+    if (moduleBase == 0) {
         std::cout << "Target process not found!\n";
         fclose(f);
         FreeConsole();
@@ -72,47 +69,39 @@ DWORD WINAPI HackThread(HMODULE hModule)
         return 0;
     }
 
-    SetInventoryItemFunc = (_SetInventoryItemFunc)(moduleBase + 0x927A0);
-    GetLocalPlayerFunc = (_GetLocalPlayerFunc)(moduleBase + 0x5AE30);
-    GetCreatureByIdFunc = (_GetCreatureByIdFunc)(moduleBase + 0x14DE10); // Point to the correct offset for getCreatureById
+    GetLocalPlayerFunc = (_GetLocalPlayerFunc)(moduleBase + OFFSET_GetLocalPlayerFunc);
+    SetInventoryItemFunc = (_SetInventoryItemFunc)(moduleBase + OFFSET_SetInventoryItemFunc);
+    GetCreatureByIdFunc = (_GetCreatureByIdFunc)(moduleBase + OFFSET_GetCreatureByIdFunc);
 
-    while (true)
-    {
-        if (GetAsyncKeyState(VK_END) & 1)
-        {
+    while (true) {
+        if (GetAsyncKeyState(VK_END) & 1) {
             break;
         }
-        if (GetAsyncKeyState(VK_NUMPAD2) & 1)
-        {
+        if (GetAsyncKeyState(VK_NUMPAD2) & 1) {
             std::cout << "NUMPAD2 pressed" << std::endl;
 
-            void* mapPtr = (void*)(moduleBase + 0x932AF0); // Assuming this is the address of the map object
-            int creatureId = 234442120; // Given creature ID
+            void* mapPtr = (void*)(moduleBase + OFFSET_g_map);
 
-            if (mapPtr) // Always check if the map pointer is valid
-            {
+            int creatureId = 331840776;
+
+            if (mapPtr && GetCreatureByIdFunc) {
                 void* creaturePtr = GetCreatureByIdFunc(mapPtr, creatureId);
-                if (creaturePtr && IsValidMemoryAddress(creaturePtr) && IsValidMemoryAddress((void*)((uintptr_t)creaturePtr + 0x08)) && IsValidMemoryAddress((void*)((uintptr_t)creaturePtr + 0x10)) && IsValidMemoryAddress((void*)((uintptr_t)creaturePtr + 0x14))) {
-          
-                    int x = *((int*)((uintptr_t)creaturePtr + 0x08)); // Hypothetical offset for x
-                    int y = *((int*)((uintptr_t)creaturePtr + 0x10)); // Given offset for y
-                    int z = *((int*)((uintptr_t)creaturePtr + 0x14)); // Given offset for z
+                if (creaturePtr && CanReadMemory(creaturePtr)) {
+                    int x = *((int*)((uintptr_t)creaturePtr + 0x08));
+                    int y = *((int*)((uintptr_t)creaturePtr + 0x10));
+                    int z = *((int*)((uintptr_t)creaturePtr + 0x14));
 
                     std::cout << "Creature Coordinates: (" << x << ", " << y << ", " << z << ")" << std::endl;
-                    Sleep(500); // Sleep for half a second
+                    Sleep(500);
                 }
-                else
-                {
-                    std::cout << "Creature not found!" << std::endl;
+                else {
+                    std::cout << "Creature not found or invalid memory!" << std::endl;
                 }
             }
-            else
-            {
-                std::cout << "Map pointer is invalid!" << std::endl;
+            else {
+                std::cout << "Map pointer is invalid or function not initialized!" << std::endl;
             }
         }
-
-
         Sleep(10);
     }
 
@@ -122,18 +111,11 @@ DWORD WINAPI HackThread(HMODULE hModule)
     return 0;
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule,
-    DWORD ul_reason_for_call,
-    LPVOID lpReserved
-)
-{
-    switch (ul_reason_for_call)
-    {
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
-    {
         CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr));
         break;
-    }
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
