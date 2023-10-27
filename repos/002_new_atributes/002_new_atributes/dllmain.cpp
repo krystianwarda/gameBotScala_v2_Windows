b33cbc538c9e135fda563516bdd05879453a5f12
krystianwarda <krystian.warda@gmail.com>
2023-10-27 16:39:36 -0700

    reverse the changes after client update

--- repos/002_new_atributes/002_new_atributes/dllmain.cpp
+++ repos/002_new_atributes/002_new_atributes/dllmain.cpp
@@ -26,20 +26,9 @@ typedef double(__thiscall* pfnGetMaxHealth)(LPVOID g_LocalPlayerPtr);
 typedef double(__thiscall* pfnGetMana)(LPVOID g_LocalPlayerPtr);
 typedef double(__thiscall* pfnGetMaxMana)(LPVOID g_LocalPlayerPtr);
 typedef double(__thiscall* pfnGetLevel)(LPVOID g_LocalPlayerPtr);
-typedef double(__thiscall* pfnGetTotalCapacity)(LPVOID g_LocalPlayerPtr);
 
-char* charNamePtr;
-unsigned char* LightModePtr;
-unsigned char* charOutfitSetPtr;
-unsigned char* charOutfitHeadColorPtr;
-unsigned char* charOutfitChestColorPtr;
-unsigned char* charOutfitLegsColorPtr;
-unsigned char* charOutfitBootsColorPtr;
-int* charXPosPtr;
-int* charYPosPtr;
-int* charZPosPtr;
-BYTE* charStanceAddress;
-BYTE* charFollowMonsterStatusAddress;
+typedef int(__thiscall* pfnGetCreatures)(LPVOID g_GamePtr);
+
 
 #pragma warning (disable:4477)
 
@@ -81,53 +70,11 @@ DWORD __stdcall BGThread(HMODULE hModule)
 
     pfnGetMaxMana getMaxMana = (pfnGetMaxMana)(g_hGameModule + 0x5AD50);
     pfnGetLevel getLevel = (pfnGetLevel)(g_hGameModule + 0x96F50);
-
-    charNamePtr = (char*)(g_hGameModule + 0x932B50);
-
     pfnGetHealth getHealth = (pfnGetHealth)(g_hGameModule + 0x96F00);
     pfnGetMaxHealth getMaxHealth = (pfnGetMaxHealth)(g_hGameModule + 0x96F10);
     pfnGetMana getMana = (pfnGetMana)(g_hGameModule + 0x5AD40);
-    pfnGetTotalCapacity getTotalCapacity = (pfnGetTotalCapacity)(g_hGameModule + 0x96F30);
+    pfnGetCreatures getCreatures = (pfnGetCreatures)(g_hGameModule + 0x1A12E0);
 
-    charXPosPtr = (int*)(g_hGameModule + 0x932D9C);
-    charYPosPtr = (int*)(g_hGameModule + 0x932DA0);
-    charZPosPtr = (int*)(g_hGameModule + 0x932DA4);
-
-    //speed
-    //DWORD baseAddr = *(DWORD*)(g_hGameModule + 0x932B70);
-    //DWORD offset1 = *(DWORD*)(baseAddr + 0x0);
-    //DWORD offset2 = *(DWORD*)(offset1 + 0x1CC);
-    //DWORD dynamicAddr = offset2 + 0xB0; // This should now contain 13E2D8C8
-
-
-    // move char direction
-    DWORD baseAddrCharMoveDirection = *(DWORD*)(g_hGameModule + 0x932C70); // Base address: "RealeraDX-1693821795.exe"+00932B70 -> 051CA9F0
-    DWORD offset1CharMoveDirection = *(DWORD*)(baseAddrCharMoveDirection + 0x0);           // Follows pointer at 051CA9F0 to reach 15F53B48
-    DWORD offset2CharMoveDirection = *(DWORD*)(offset1CharMoveDirection + 0x14);           // Follows pointer at 15F53B48 + 0x14 to reach 14502030
-    DWORD dynamicAddrCharMoveDirection = offset2CharMoveDirection + 0x4C;
-
-
-    // character name and light
-    DWORD baseAddress = *(DWORD*)(g_hGameModule + 0x933DA8);   // Step 1
-    DWORD offset1 = *(DWORD*)(baseAddress + 0xB8);   // Step 2
-    DWORD offset2 = *(DWORD*)(offset1 + 0x84);   // Step 3
-    DWORD offset3 = *(DWORD*)(offset2 + 0x0);   // Step 4
-    DWORD offset4 = *(DWORD*)(offset3 + 0x0);   // Step 5
-    DWORD offset5 = *(DWORD*)(offset4 + 0x36C);   // Step 6
-    DWORD offset6 = *(DWORD*)(offset5 + 0x1CC);   // Step 7
-    LightModePtr = (unsigned char*)(offset6 + 0xAD);
-
-    // Outfit related dynamic addresses
-    charOutfitSetPtr = (unsigned char*)(offset6 + 0x58);
-    charOutfitHeadColorPtr = (unsigned char*)(offset6 + 0x60);
-    charOutfitChestColorPtr = (unsigned char*)(offset6 + 0x64);
-    charOutfitLegsColorPtr = (unsigned char*)(offset6 + 0x68);
-    charOutfitBootsColorPtr = (unsigned char*)(offset6 + 0x6C);
-    charStanceAddress = (BYTE*)(g_hGameModule + 0x932B24);
-    charFollowMonsterStatusAddress = (BYTE*)(g_hGameModule + 0x932B28);
-    int* currentBattleTargetIDPtr = (int*)(g_hGameModule + 0x932AA4);
-
-    // Calculate the dynamic address for lightMode based on characterName's dynamic address
 
 
 
@@ -135,22 +82,8 @@ DWORD __stdcall BGThread(HMODULE hModule)
 
     while (1) {
 
-        int charXPos = *charXPosPtr;
-        int charYPos = *charYPosPtr;
-        int charZPos = *charZPosPtr;
-        std::string charName = charNamePtr;
-        //int valueAtDynamicAddress = *(int*)(dynamicAddr);
-        BYTE charMoveDirection = *(BYTE*)(dynamicAddrCharMoveDirection);
-        int LightMode = static_cast<int>(*LightModePtr);
-        int charOutfitSet = static_cast<int>(*charOutfitSetPtr);
-        int charOutfitHeadColor = static_cast<int>(*charOutfitHeadColorPtr);
-        int charOutfitChestColor = static_cast<int>(*charOutfitChestColorPtr);
-        int charOutfitLegsColor = static_cast<int>(*charOutfitLegsColorPtr);
-        int charOutfitBootsColor = static_cast<int>(*charOutfitBootsColorPtr);
-        BYTE charStance = *charStanceAddress;
-        BYTE charFollowMonsterStatus = *charFollowMonsterStatusAddress;
-        int currentBattleTargetID = *currentBattleTargetIDPtr;
 
+        int firstCreatureID = getCreatures(g_Game);
         system("cls"); // Clear the console before every loop
 
 
@@ -159,32 +92,13 @@ DWORD __stdcall BGThread(HMODULE hModule)
         nlohmann::json gameData;
         gameData["Game_Base"] = g_hGameModule;
         gameData["Game_Pointer"] = (int)g_Game;
-
-        gameData["Character_Name"] = charName;
-        
+ 
         gameData["Health"] = { {"current", (int)getHealth(g_Player)}, {"max", (int)getMaxHealth(g_Player)} };
         gameData["Mana"] = { {"current", (int)getMana(g_Player)}, {"max", (int)getMaxMana(g_Player)} };
         gameData["Level"] = (int)getLevel(g_Player);
-        gameData["Total_Capacity"] = (int)getTotalCapacity(g_Player);
-       // gameData["Char_Speed"] = valueAtDynamicAddress;
+        gameData["FirstCreatureID"] = firstCreatureID;
 
-        gameData["Char_X_Position"] = charXPos;
-        gameData["Char_Y_Position"] = charYPos;
-        gameData["Char_Z_Position"] = charZPos;
-        gameData["Character_Move_Direction"] = charMoveDirection;
-        gameData["Light_mode"] = LightMode;
-        gameData["charOutfit"] = { 
-            {"charOutfitSet", charOutfitSet},
-            {"charOutfitHeadColor", charOutfitHeadColor},
-            {"charOutfitChestColor", charOutfitChestColor},
-            {"charOutfitLegsColor", charOutfitLegsColor},
-            {"charOutfitBootsColor", charOutfitBootsColor} 
-        };
-        gameData["Char_Stance"] = charStance;
-        gameData["Char_FollowMonsterStatus"] = charFollowMonsterStatus;
-        gameData["Char_Target_Id"] = currentBattleTargetID;
-
-
+ 
         // Serialize the JSON and save to file
         std::ofstream outFile("gameData.json");
         outFile << gameData.dump(4);  // dump(4) adds 4-space indentation
@@ -235,7 +149,7 @@ int __stdcall DllMain(HMODULE hModule, DWORD fdwReason, LPVOID reserved)
             }
         }
 
-        g_Game = (LPDWORD)(g_hGameModule + 0x932AA0);
+        g_Game = (LPDWORD)(g_hGameModule + 0x930D00);
 
         CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)BGThread, hModule, 0, 0));
     }
--- repos/003_send_data/003_send_data/dllmain.cpp
+++ repos/003_send_data/003_send_data/dllmain.cpp
@@ -3,42 +3,24 @@
 #include <iostream>
 #include <TlHelp32.h>
 #include <string>
+#include "player.h" // Make sure this path is correct
 
-bool CanReadMemory(void* address) {
-    __try {
-        volatile char check = *(char*)address;
-        return true;
-    }
-    __except (EXCEPTION_EXECUTE_HANDLER) {
-        return false;
-    }
-}
-
-class Item {
+class LocalPlayer : public Player {
+    // Implementation of LocalPlayer if required
 public:
-    int m_position_x;
-    int m_position_y;
-    short m_position_z;
-    unsigned short m_datId;
-    int itemId;
-
-    Item(int id) : itemId(id) {}
+    LocalPlayer() { /* Constructor Implementation if required */ }
+    double getHealth() { 
+        // Implementation to get health.
+        // You need to define how health is retrieved in your game
+        return 100.0; // Example value
+    }
 };
 
-typedef void* (__thiscall* _GetCreatureByIdFunc)(void* mapPtr, int creatureId);
-typedef void(__thiscall* _SetInventoryItemFunc)(void* localPlayerPtr, int slot, const Item& item);
-typedef void* (__thiscall* _GetLocalPlayerFunc)(void* gamePtr);
-
+typedef LocalPlayer*(__fastcall* _GetLocalPlayerFunc)(void* g_game_ptr);
 _GetLocalPlayerFunc GetLocalPlayerFunc;
-_SetInventoryItemFunc SetInventoryItemFunc;
-_GetCreatureByIdFunc GetCreatureByIdFunc;
 
-const uintptr_t OFFSET_SetInventoryItemFunc = 0x927A0 + 0x110;
-const uintptr_t OFFSET_GetLocalPlayerFunc = 0x5AE30 + 0x110;
-const uintptr_t OFFSET_GetCreatureByIdFunc = 0x14DE10 + 0x110;
-const uintptr_t OFFSET_g_map = 0x932AA0 + 0x160;
-
-DWORD WINAPI HackThread(HMODULE hModule) {
+DWORD WINAPI HackThread(HMODULE hModule)
+{
     AllocConsole();
     FILE* f;
     freopen_s(&f, "CONOUT$", "w", stdout);
@@ -47,12 +29,16 @@ DWORD WINAPI HackThread(HMODULE hModule) {
 
     uintptr_t moduleBase = 0;
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (hSnapshot != INVALID_HANDLE_VALUE) {
+    if (hSnapshot != INVALID_HANDLE_VALUE)
+    {
         PROCESSENTRY32 pe32;
         pe32.dwSize = sizeof(PROCESSENTRY32);
-        if (Process32First(hSnapshot, &pe32)) {
-            do {
-                if (wcsstr(pe32.szExeFile, L"eale")) {
+        if (Process32First(hSnapshot, &pe32))
+        {
+            do
+            {
+                if (wcsstr(pe32.szExeFile, L"eale")) // Check if the process name contains "eale"
+                {
                     moduleBase = (uintptr_t)GetModuleHandleW(pe32.szExeFile);
                     break;
                 }
@@ -61,7 +47,8 @@ DWORD WINAPI HackThread(HMODULE hModule) {
         CloseHandle(hSnapshot);
     }
 
-    if (moduleBase == 0) {
+    if (moduleBase == 0)
+    {
         std::cout << "Target process not found!\n";
         fclose(f);
         FreeConsole();
@@ -69,36 +56,25 @@ DWORD WINAPI HackThread(HMODULE hModule) {
         return 0;
     }
 
-    GetLocalPlayerFunc = (_GetLocalPlayerFunc)(moduleBase + OFFSET_GetLocalPlayerFunc);
-    SetInventoryItemFunc = (_SetInventoryItemFunc)(moduleBase + OFFSET_SetInventoryItemFunc);
-    GetCreatureByIdFunc = (_GetCreatureByIdFunc)(moduleBase + OFFSET_GetCreatureByIdFunc);
+    GetLocalPlayerFunc = (_GetLocalPlayerFunc)(moduleBase + 0x5AE30);
 
-    while (true) {
-        if (GetAsyncKeyState(VK_END) & 1) {
+    while (true)
+    {
+        if (GetAsyncKeyState(VK_END) & 1)
+        {
             break;
         }
-        if (GetAsyncKeyState(VK_NUMPAD2) & 1) {
-            std::cout << "NUMPAD2 pressed" << std::endl;
 
-            void* mapPtr = (void*)(moduleBase + OFFSET_g_map);
-
-            int creatureId = 338554048;
-
-            if (mapPtr && GetCreatureByIdFunc) {
-                void* creaturePtr = GetCreatureByIdFunc(mapPtr, creatureId);
-                if (creaturePtr && CanReadMemory(creaturePtr)) {
-                    int x = *((int*)((uintptr_t)creaturePtr + 0x08));
-                    int y = *((int*)((uintptr_t)creaturePtr + 0x10));
-                    int z = *((int*)((uintptr_t)creaturePtr + 0x14));
-
-                    std::cout << "Creature Coordinates: (" << x << ", " << y << ", " << z << ")" << std::endl;
-                }
-                else {
-                    std::cout << "Creature not found or invalid memory!" << std::endl;
-                }
-            }
-            else {
-                std::cout << "Map pointer is invalid or function not initialized!" << std::endl;
+        if (GetAsyncKeyState(VK_NUMPAD2) & 1)
+        {
+            std::cout << "Button pressed" << std::endl;
+            
+            LocalPlayer* localPlayer = GetLocalPlayerFunc((void*)(moduleBase + 0x932990));
+            if(localPlayer != nullptr) {
+                double health = localPlayer->getHealth();
+                std::cout << "Local Player Health: " << health << std::endl;
+            } else {
+                std::cout << "Local player not found!" << std::endl;
             }
         }
         Sleep(10);
@@ -110,8 +86,13 @@ DWORD WINAPI HackThread(HMODULE hModule) {
     return 0;
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-    switch (ul_reason_for_call) {
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD ul_reason_for_call,
+    LPVOID lpReserved
+)
+{
+    switch (ul_reason_for_call)
+    {
     case DLL_PROCESS_ATTACH:
         CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr));
         break;
