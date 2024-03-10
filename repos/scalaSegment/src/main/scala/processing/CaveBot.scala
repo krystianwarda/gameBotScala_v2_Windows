6540c4aa1e234321270249baa0651320c16f2710
krystianwarda <krystian.warda@gmail.com>
2024-03-10 22:27:35 +0100

    cavebot v3

--- repos/scalaSegment/src/main/scala/processing/CaveBot.scala
+++ repos/scalaSegment/src/main/scala/processing/CaveBot.scala
@@ -3,6 +3,8 @@ package processing
 import mouse.FakeAction
 import play.api.libs.json.JsValue
 import userUI.SettingsUtils
+import play.api.libs.json._
+
 
 object CaveBot {
   def computeCaveBotActions(json: JsValue, settings: SettingsUtils.UISettings): (Seq[FakeAction], Seq[Log]) = {
@@ -11,17 +13,46 @@ object CaveBot {
     println("Performing computeCaveBotActions action.")
 
     if (settings.caveBotSettings.enabled) {
-      // Convert JList model to Scala collection
-      val waypointsModel = settings.caveBotSettings.waypointsList.getModel
-      val waypointsCount = waypointsModel.getSize
-      println(s"Number of waypoints: $waypointsCount")
+      println("caveBotSettings enabled.")
 
-      // Use asScala to convert Java enumeration to Scala collection
-      for (index <- 0 until waypointsCount) {
-        val waypoint = waypointsModel.getElementAt(index)
-        println(waypoint)
+      // Define a function to execute when there are no monsters or battleInfo is null
+      def executeWhenNoMonstersOrInvalidBattleInfo(): Unit = {
+        println("Proceeding with actions because either no monster is in battle or battleInfo is null.")
+        val presentCharLocationX = (json \ "characterInfo" \ "PositionX").as[Int]
+        val presentCharLocationY = (json \ "characterInfo" \ "PositionY").as[Int]
+        val presentCharLocationZ = (json \ "characterInfo" \ "PositionZ").as[Int]
+
+        println(s"Type of waypointsList: ${settings.caveBotSettings.waypointsList.getClass}")
+
+        // Convert JList model to Scala collection
+        val waypointsModel = settings.caveBotSettings.waypointsList.getModel
+        val waypointsCount = waypointsModel.getSize
+        println(s"Number of waypoints: $waypointsCount")
+
+        // Use asScala to convert Java enumeration to Scala collection
+        for (index <- 0 until waypointsCount) {
+          val waypoint = waypointsModel.getElementAt(index)
+          println(waypoint)
+        }
       }
-    } else None
+
+      // Safely attempt to parse battleInfo as a map
+      val battleInfoResult = (json \ "battleInfo").validate[Map[String, JsValue]]
+
+      battleInfoResult match {
+        case JsSuccess(battleInfo, _) =>
+          val hasMonsters = battleInfo.exists { case (_, creature) =>
+            (creature \ "IsMonster").asOpt[Boolean].getOrElse(false)
+          }
+          if (!hasMonsters) {
+            executeWhenNoMonstersOrInvalidBattleInfo()
+          } else {
+            println("Skipping actions due to presence of monsters.")
+          }
+        case JsError(_) =>
+          executeWhenNoMonstersOrInvalidBattleInfo()
+      }
+    }
 
     (actions, logs)
   }
